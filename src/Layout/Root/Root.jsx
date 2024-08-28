2ca307e23596999d446e708b3e5236734efb1d2d
dev-kawser <kawserofficial.dev@gmail.com>
2024-08-28 17:03:45 +0600

    navbar added with responsive

--- src/Layout/Root/Root.jsx
+++ src/Layout/Root/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../../Shared/Navbar/Navbar";
 
 
 const Root = () => {
     return (
-        <div>
+        <div className="inter">
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
